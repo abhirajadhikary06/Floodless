81e64087714d40ad95c21e890d37bfb5801327a7
Abhiraj Adhikary <abhirajadhikary06@gmail.com>
2025-03-21 16:16:39 +0530

    added dashboard app

--- floodless/dashboard/models.py
+++ floodless/dashboard/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Calamity(models.Model):
     year = models.IntegerField(null=True, blank=True)
-    calamity_type = models.CharField(max_length=100, null=True, blank=True)  # e.g., "earthquake"
+    calamity_type = models.CharField(max_length=100, null=True, blank=True)
     country = models.CharField(max_length=100, null=True, blank=True)
     region = models.CharField(max_length=255, null=True, blank=True)
     location = models.CharField(max_length=255, null=True, blank=True)
